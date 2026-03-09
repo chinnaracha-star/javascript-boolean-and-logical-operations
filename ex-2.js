54da24fed09313b2ca438d57f963a764366cb5db
Chindurex <chinnaracha@gmail.com>
2026-03-09 13:42:08 +0700

    docs: answer ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,21 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = true;
+let isWeekday = true;
+let hasBoughtProductFromITCategory = false;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = false;
 
-let hasPromotion;
+// กรณีที่ 1: ผู้ซื้อมียอดสั่งซื้อสินค้าจากเดือนที่แล้วมากกว่า 4000 บาท และวันที่ซื้อสินค้าเป็นวันจันทร์ถึงวันศุกร์ 
+// และไม่เคยซื้อสินค้าจากหมวดหมู่ IT มาก่อน และไม่เคยเข้าร่วมกิจกรรมมหกรรมลดราคามาก่อน
+// กรณีที่ 2 : ผู้ซื้อเป็น Member ในระดับ Platinum
 
+
+let hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory &&
+     !hasAttendedDiscountEvent) || isPlatinum;
+
+    //true && true && true && false
+    //false
+    //false || false 
 console.log(hasPromotion);
